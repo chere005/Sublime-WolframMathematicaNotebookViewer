516c4e459f2d9060b9e27be3e697b0831149ddea
chere005 <chere005@users.noreply.github.com>
2017-06-14 20:52:30 -0500

    Remove mPath

--- NotebookView.py
+++ NotebookView.py
@@ -4,7 +4,7 @@ class NotebookviewCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#Import Current Filepath as Plaintext
 		path = str(self.view.file_name());
-		nbText = os.popen(mPath + 'wolframscript -code "Quiet@Import[\\"' + path + '\\", {\\"NB\\", \\"Plaintext\\"}]"').read()
+		nbText = os.popen('wolframscript -code "Quiet@Import[\\"' + path + '\\", {\\"NB\\", \\"Plaintext\\"}]"').read()
 
 		if(nbText != "$Failed\n"):
 			self.view.window().new_file().insert(edit, 0, nbText)
